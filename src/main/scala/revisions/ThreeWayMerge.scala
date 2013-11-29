5fd864c7b2d8dbf983a5a6642bb5a3d3b8362cca
Daniel James <dwhjames@gmail.com>
2013-11-29 13:41:25 -0500

    simplify merge computation when main has not written since root
    main has not written to the revisioned value since the root if getIndex(main) == getIndex(root), therefore we only need to merge in the value from the branch, saving the computation the three-way merge

--- src/main/scala/revisions/ThreeWayMerge.scala
+++ src/main/scala/revisions/ThreeWayMerge.scala
@@ -5,11 +5,17 @@ trait ThreeWayMerge[T] extends AbstractVersioned[T] {
   protected def mergeValue(root: T, main: T, join: T): T
 
   override protected def computeMerge(main: Revision[_], joinRev: Revision[_], join: Segment): T = {
-    val idx = getIndex(main.current)
-    if (idx < 0)
+    val mainIdx = getIndex(main.current)
+    if (mainIdx < 0)
       versions(join.version)
-    else
-      mergeValue(get(joinRev.root), versions(idx), versions(join.version))
+    else {
+      // mainIdx is non-negative, so rootIdx must be as well
+      val rootIdx = getIndex(joinRev.root)
+      if (rootIdx == mainIdx) // if main has not written since the root
+        versions(join.version)
+      else
+        mergeValue(versions(rootIdx), versions(mainIdx), versions(join.version))
+    }
   }
 
 }
